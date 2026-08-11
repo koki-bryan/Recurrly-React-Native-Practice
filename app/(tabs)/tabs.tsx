419a49d069f7aef4557c6aba541bd2c76c8b9f92
yhwh <seanbryan.noces@gmail.com>
2026-08-12 04:13:17 +0800

    feat: initial commit, learning react native foundations and boilerplate

---
+++ app/(tabs)/tabs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const tabs = () => {
+  return (
+    <View>
+      <Text>tabs</Text>
+    </View>
+  );
+};
+
+export default tabs;
